e82b3275a57f027e6532045137d386200d262a69
Amr Hassan Hafez <amrhassan98@hotmail.com>
2020-07-14 00:32:35 +0200

    added two function
    added two function: 1- "scrollFunction" 2- "nav_scroll" status: ongoing

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,43 @@ function changeClass() {
   }
 }
 
+// navbar scroll view
+// status: ongoing
+// var prevScrollpos = window.pageYOffset;
+// function nav_scroll() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-60px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+//button code
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function go_top() {scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+window.onscroll = function() {
+  // nav_scroll();
+  scrollFunction();
+}
+
